35b38e238048b9ee944d9f5c310402b5b0c76bc9
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-31 18:25:34 -0700

    Added function for displaying the current weather.

--- script.js
+++ script.js
@@ -26,3 +26,33 @@ const forecastHum = {};
 const today = moment().format('MM' + "/" + 'DD' + '/' + 'YYYY');
 const APIKey = "&units=imperial&APPID=209189943a26243e19e862011b35aa5e";
 const url =  "https://api.openweathermap.org/data/2.5/weather?q=";
+
+// Display current weather of a city
+function currentWeather(userInput) {
+    mainIcon.empty(); // empty all child elements when function is ran
+    const queryURL = url + userInput + APIKey;
+    fetch (queryURL, { // fetch the city that was searched
+    })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (data) { // grab the weather data from the API call
+            let cityInfo = data.name;
+            let country = data.sys.country; 
+            let temp = data.main.temp;
+            let humidity = data.main.humidity;
+            let wind = data.wind.speed;
+            let description = data.weather[0].description;
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+            let icon = data.weather[0].icon;
+            let uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + lat + "&lon=" + lon + "&APPID=209189943a26243e19e862011b35aa5e"; // create uv index API url from the lat & lon data
+            let mainImage = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png"); // create image URL via https://openweathermap.org/weather-conditions
+            mainIcon.append(mainImage);
+            cityText.text(cityInfo + ", " + country + " " + today);
+            tempText.text("Temperature: " + Math.round(temp) + " ºF"); // Round the temperature for readability
+            tempDescription.text(description);
+            humidityText.text("Humidity: " + humidity + " %");
+            windText.text("Wind Speed: " + wind + " MPH");
+        });
+}
